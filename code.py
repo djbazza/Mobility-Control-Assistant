0f47aee1cc37a958264b0951296e60d1b7e41291
davidbarrett@ihug.com.au <davidb@pw.local>
2024-09-05 13:10:05 +1000

    Thumbstick Customisation

--- code.py
+++ code.py
@@ -28,18 +28,18 @@ pool = socketpool.SocketPool(wifi.radio)
 server = Server(pool, "/static", debug=True)
 
 sampling_ms = 150
-InitRange = 125
+minChange = 5
 
 # Axis configuration constants
-AXIS_DB = 0
+AXIS_DB = 5
 AXIS_MIN = -255  # Minimum raw axis value.
 AXIS_MAX = 255 # Maximum raw axis value.
+factor = 25
 
 # Axis configuration constants
-factor = 25
-ACCEL_MIN = -8*factor  # Minimum raw axis value.
-ACCEL_MAX = 8*factor # Maximum raw axis value.
-ACCEL_DB = int((ACCEL_MIN + ACCEL_MAX)/2) # Deadband to apply to axis center points.
+THUMB_MIN = -128  # Minimum raw axis value.
+THUMB_MAX = 128 # Maximum raw axis value.
+THUMB_DB = 0 # Deadband to apply to axis center points.
 
 # IMU6866 define
 MPU6886_ADDRESS=0x68
@@ -61,10 +61,10 @@ hotas = Joystick()
 
 hotas.add_input(
     Button(),
-    Axis(deadband=AXIS_DB, min=AXIS_MIN, max=AXIS_MAX),
-    Axis(deadband=AXIS_DB, min=AXIS_MIN, max=AXIS_MAX),
-    Axis(deadband=ACCEL_DB, min=ACCEL_MIN, max=ACCEL_MAX),
-    Axis(deadband=ACCEL_DB, min=ACCEL_MIN, max=ACCEL_MAX),
+    Axis(deadband=125, min=-255, max=255),
+    Axis(deadband=125, min=-255, max=255),
+    Axis(deadband=THUMB_DB, min=THUMB_MIN, max=THUMB_MAX),
+    Axis(deadband=THUMB_DB, min=THUMB_MIN, max=THUMB_MAX),
 )
 
 
@@ -82,10 +82,10 @@ FORM_HTML_TEMPLATE = """
             <input type="range" min="-255" max="255" value="{MinValDisp}" id="MinVal" name="MinVal">
             <p>Maximum Range:<span>{MaxValDisp}</span></p>
             <input type="range" min="-255" max="255" value="{MaxValDisp}" id="MaxVal" name="MaxVal">
-            <p>Motion Range:<span id="ra">{RangeVal}</span></p>
-            <input type="range" min="1" max="255" value="{RangeVal}" id="Range" name="Range">
+            <p>Minimum Motion Change:<span id="ra">{ChangeVal}</span></p>
+            <input type="range" min="1" max="50" value="{ChangeVal}" id="Change" name="Change">
             <p>Set Deadband:<span id="dB">{DeadbandVal}</span></p>
-            <input type="range" min="-255" max="255" value="{DeadbandVal}" id="Deadband" name="Deadband">
+            <input type="range" min="0" max="100" value="{DeadbandVal}" id="Deadband" name="Deadband">
             <p>Motion Options</p>
             <input type="radio" name="Motion" id="RelativeMotion" value="RelativeMotion" checked="checked"> Relative Motion
             <input type="radio" name="Motion" id="ChangeInMotion" value="ChangeInMotion"> Change in Motion<br>
@@ -105,14 +105,16 @@ def form(request: Request):
     global sampling_ms
     global AXIS_MIN # Minimum raw axis value.
     global AXIS_MAX # Maximum raw axis value.
+    global AXIS_DB # Deadband Change minimum.
+    global minChange
     enctype = request.query_params.get("enctype", "text/plain")
 
     if request.method == POST:
         sampling_ms = int(request.form_data.get("Sample"))
         AXIS_MIN = int(request.form_data.get("MinVal"))
         AXIS_MAX = int(request.form_data.get("MaxVal"))
-        posted_Range = request.form_data.get("Range")
-        posted_Deadband = request.form_data.get("Deadband")
+        minChange = int(request.form_data.get("Change"))
+        AXIS_DB = int(request.form_data.get("Deadband"))
         posted_Motion = request.form_data.get("Motion")
 
     return Response(
@@ -120,7 +122,8 @@ def form(request: Request):
         FORM_HTML_TEMPLATE.format(
             enctype=enctype,
             submitted_value=(
-                f"<h3>Submitted form Sample: {sampling_ms}ms</h3>\n<h3>Submitted form Range: {posted_Range}</h3>\n<h3>Submitted form Deadband: {posted_Deadband}</h3>\n<h3>Submitted form motion: {posted_Motion}</h3>"
+                "<h3>Updated</h3>"
+                # f"<h3>Submitted form Sample: {sampling_ms}ms</h3>\n<h3>Submitted form Range: {minChange}</h3>\n<h3>Submitted form Deadband: {posted_Deadband}</h3>\n<h3>Submitted form motion: {posted_Motion}</h3>"
                 if request.method == POST
                 else ""
             ),
@@ -139,13 +142,13 @@ def form(request: Request):
                 if request.method == POST
                 else f"{sampling_ms}"
             ),
-            RangeVal=(
-                f"{posted_Range}"
+            ChangeVal=(
+                f"{minChange}"
                 if request.method == POST
-                else f"{InitRange}"
+                else f"{minChange}"
             ),
             DeadbandVal=(
-                f"{posted_Deadband}"
+                f"{AXIS_DB}"
                 if request.method == POST
                 else f"{AXIS_DB}"
             ),
@@ -157,33 +160,51 @@ def form(request: Request):
 server.start(str(wifi.radio.ipv4_address_ap))
 
 while True:
-    
+
     try:
         gyro_array = sensor.gyro()
         accel_array = sensor.acceleration()
+        DB = AXIS_MIN + (AXIS_MAX - AXIS_MIN)/2
         X = int(accel_array[0]*factor)
         if X > AXIS_MIN and X < AXIS_MAX:
-            print(f"X: {X}")
-            hotas.axis[0].source_value = X
+            if X > DB-AXIS_DB and X < DB+AXIS_DB:
+                hotas.axis[0].source_value = 125
+            # print(f"X: {X}")
+            elif X < hotas.axis[0].source_value - minChange or X > hotas.axis[0].source_value + minChange:
+                hotas.axis[0].source_value = X
         Y = 0-int(accel_array[1]*factor)
         if Y > AXIS_MIN and Y < AXIS_MAX:
-            print(f"Y: {Y}")
-            hotas.axis[1].source_value = Y
+            if Y > DB-AXIS_DB and Y < DB+AXIS_DB:
+                hotas.axis[1].source_value = 125
+            elif Y < hotas.axis[1].source_value - minChange or Y > hotas.axis[1].source_value + minChange:
+                hotas.axis[1].source_value = Y
         if jhat:
-            hotas.axis[2].source_value = jhat.get_x()
-            hotas.axis[3].source_value = jhat.get_y()
-            hotas.button[0].source_value = 1 - jhat.get_button_status()
+            X = jhat.get_x()
+            if X > AXIS_MIN/2 and X < AXIS_MAX/2:
+                if X > DB/2-AXIS_DB and X < DB/2+AXIS_DB:
+                    hotas.axis[2].source_value = 0
+                    # print(f"X: {X}")
+                elif X < hotas.axis[2].source_value - minChange or X > hotas.axis[2].source_value + minChange:
+                    hotas.axis[2].source_value = X
+            Y = jhat.get_y()
+            if Y > AXIS_MIN/2 and Y < AXIS_MAX/2:
+                if Y > DB/2-AXIS_DB and Y < DB/2+AXIS_DB:
+                    hotas.axis[3].source_value = 0
+                    # print(f"X: {X}")
+                elif Y < hotas.axis[3].source_value - minChange or Y > hotas.axis[3].source_value + minChange:
+                    hotas.axis[3].source_value = Y
+                hotas.button[0].source_value = 1 - jhat.get_button_status()
         hotas.update()
         # print(jhat.get_x())
-        #print(accel_array)
-        
+        # print(accel_array)
+
                 # Process any waiting requests
         pool_result = server.poll()
-        
+
         if pool_result == REQUEST_HANDLED_RESPONSE_SENT:
             # Do something only after handling a request
             pass
-            
+
         time.sleep(sampling_ms/1000)
 
         # If you want you can stop the server by calling server.stop() anywhere in your code
